51464aaa486bbfeedc1d30076eeed22896d1d091
OlgaDobrodey <olga.gurkovskaya@gmail.com>
2021-09-12 16:33:46 +0300

    homeWork2

--- HomeWork2/src/Task3/ExceptionHandler.java
+++ HomeWork2/src/Task3/ExceptionHandler.java
@@ -3,13 +3,12 @@ package Task3;
 public class ExceptionHandler {
 
     public void OutOfMemoryErrorMetaspace(ExceptionProducer producer) {
-        while (true) {
-            try {
-                producer.OutOfMemoryErrorMetaspace();
-            } catch (OutOfMemoryError ex) {
-                Logger.getLogger().log(ex);
-                System.out.println("Произошла ошибка. Информация в файле log.txt");
-            }
+        try {
+            producer.OutOfMemoryErrorMetaspace();
+        } catch (OutOfMemoryError ex) {
+            Logger.getLogger().log(ex);
+            System.out.println("Произошла ошибка. Информация в файле log.txt");
         }
     }
 }
+
