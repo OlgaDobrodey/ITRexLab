6afb405d4b663ac12d9ec8d1ee96cf906eda32fb
OlgaDobrodey <olga.gurkovskaya@gmail.com>
2021-09-23 00:32:11 +0300

    HomeWork5 variable 3 without tests

--- src/main/java/com/itrex/java/lab/HomeWork5/Flag.java
+++
@@ -1,9 +0,0 @@
-package com.itrex.java.lab.HomeWork5;
-
-public class Flag {
-    public static boolean flag = true;
-
-    public static boolean getFlagFalse(){
-        return flag=false;
-    }
-}
--- src/main/java/com/itrex/java/lab/HomeWork5/LandRover.java
+++ src/main/java/com/itrex/java/lab/HomeWork5/LandRover.java
@@ -1,84 +1,100 @@
 package com.itrex.java.lab.HomeWork5;
 
+import java.util.concurrent.Semaphore;
 import java.util.function.IntConsumer;
 
-public class LandRover extends TaskConcurrency {
+public class LandRover implements Runnable {
 
     private volatile int count;
     private final int n;
-    private final static Integer monitor = 0;
+    private Semaphore semaphore = new Semaphore(1);
 
-    public LandRover(int n) {
-        super(n);                       //1 <= n <= 50
+    private Runnable printLand = () -> System.out.print("Land");
+    private Runnable printRover = () -> System.out.print("Rover");
+    private Runnable printLandRover = () -> System.out.print("LandRover");
+    private IntConsumer printNumber = value -> System.out.print(value);
+
+    public LandRover(int n) {   //1 <= n <= 50
         if (n < 1 || n > 50) {
             throw new IllegalArgumentException("invalid t value n");
         }
         this.n = n;
-        this.count = 1;;
+        this.count = 1;
     }
 
     static {
         System.out.print("[");
     }
 
-    @Override
     public void land(Runnable printLand) throws InterruptedException {
-        synchronized (monitor) {
-            if (count % 3 == 0 && count % 5 != 0) {
+        synchronized (this) {
+            if (count <= n && count % 3 == 0 && count % 5 != 0) {
                 printLand.run();
                 printСorrect(count);
-                setCount(count);
+                count++;
             }
         }
     }
 
-    @Override
     public void rover(Runnable printRover) throws InterruptedException {
-        synchronized (monitor) {
-            if (count % 3 != 0 && count % 5 == 0) {
+        synchronized (this) {
+            if (count <= n && count % 3 != 0 && count % 5 == 0) {
                 printRover.run();
                 printСorrect(count);
-                setCount(count);
+                count++;
             }
         }
     }
 
-    @Override
     public void landrover(Runnable printLandRover) throws InterruptedException {
-        synchronized (monitor) {
-            if (count % 3 == 0 && count % 5 == 0) {
+        synchronized (this) {
+            if (count <= n && count % 3 == 0 && count % 5 == 0) {
                 printLandRover.run();
                 printСorrect(count);
-                setCount(count);
+                count++;
             }
         }
     }
 
-    @Override
     public void number(IntConsumer printNumber) throws InterruptedException {
-        synchronized (monitor) {
-            if (count % 3 != 0 && count % 5 != 0) {
+        synchronized (this) {
+            if (count <= n && count % 3 != 0 && count % 5 != 0) {
                 printNumber.accept(count);
                 printСorrect(count);
-                setCount(count);
+                count++;
             }
         }
-    }
-
-    //method set count and if count = n stops threads
-    private void setCount(int count) {
-        if ((count + 1) > n) {
-            Flag.getFlagFalse();
-            this.count = count;
-        } else this.count = count + 1;
     }
 
     //method correct print to view ["land",2,3]
     private void printСorrect(int count) {
-        if ((count + 1) > n) {
+        if (count == n) {
             System.out.println("]");
         } else {
             System.out.print(",");
         }
     }
+
+    @Override
+    public void run() {
+        while (count <= n) {
+            try {
+                semaphore.acquire();
+                if (Thread.currentThread().getName().equals("thread-a")) {
+                    land(printLand);
+                } else if (Thread.currentThread().getName().equals("thread-b")) {
+                    rover(printRover);
+                } else if (Thread.currentThread().getName().equals("thread-c")) {
+                    landrover(printLandRover);
+                } else {
+                    number(printNumber);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                semaphore.release();
+            }
+        }
+    }
 }
+
